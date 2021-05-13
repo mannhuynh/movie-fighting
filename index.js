b7b093acb6ddd2f5147d81829e31314c167d2bb9
Vuong Huynh <42597483+mannhuynh@users.noreply.github.com>
2021-05-13 12:05:32 -0500

    Update http to https for all API links.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const autoCompleteConfig = {
 	},
 
 	async fetchData(searchTerm) {
-		const response = await axios.get("http://www.omdbapi.com/", {
+		const response = await axios.get("https://www.omdbapi.com/", {
 			params: {
 				apikey: "c3c09d02",
 				s: searchTerm,
@@ -47,7 +47,7 @@ createAutoComplete({
 let leftMovie;
 let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
-	const response = await axios.get("http://www.omdbapi.com/", {
+	const response = await axios.get("https://www.omdbapi.com/", {
 		params: {
 			apikey: "c3c09d02",
 			i: movie.imdbID,
